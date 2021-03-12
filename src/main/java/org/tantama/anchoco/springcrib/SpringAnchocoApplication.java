34aee70842cf55346ed20c9809b544c7c799fa54
新田八大 <@LAPTOP-U65PSE3J>
2021-03-12 15:47:54 +0900

    initialize project.

---
+++ src/main/java/org/tantama/anchoco/springcrib/SpringAnchocoApplication.java
@@ -0,0 +1,13 @@
+package org.tantama.anchoco.springcrib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnchocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnchocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tantama/anchoco/springcrib/SpringAnchocoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tantama.anchoco.springcrib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnchocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
